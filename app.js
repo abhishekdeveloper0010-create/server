e326b1aa705b3255130102c10d9cda584bc00220
abhishek.developer0010@gmail.com <abhishekkumar97831@gmail.com>
2026-08-12 09:35:58 +0530

    get server deta

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const path =  require("path");
+
 require("dotenv").config();
 
 require("./config/db");
@@ -15,6 +17,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/images",express.static(path.join(__dirname, "uploads/products")));
 
 createUsersTable();
 
