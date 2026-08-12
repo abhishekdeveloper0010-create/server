976250216cf7cf8cd1540e29771fcca28217dc8a
abhishek.developer0010@gmail.com <abhishekkumar97831@gmail.com>
2026-08-12 17:01:37 +0530

    add category API

--- app.js
+++ app.js
@@ -1,30 +1,61 @@
 const express = require("express");
 const cors = require("cors");
-const path =  require("path");
+const path = require("path");
 
 require("dotenv").config();
-
 require("./config/db");
+
 const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
-const { createUsersTable } = require("./models/userModel");
+const categoryRoutes = require("./routes/categoryRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 4000;
+
+// ==============================
+// MIDDLEWARE
+// ==============================
 
 app.use(cors({ origin: "*" }));
+
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/products", productRoutes);
-app.use("/images",express.static(path.join(__dirname, "uploads/products")));
+// ==============================
+// API ROUTES
+// ==============================
 
-createUsersTable();
+app.use("/api/auth", authRoutes);
+
+app.use("/api/products", productRoutes);
+
+app.use("/api/categories", categoryRoutes);
+
+// ==============================
+// PRODUCT IMAGES
+// ==============================
+
+app.use("/images", express.static(path.join(__dirname, "uploads/products")));
+
+// ==============================
+// CATEGORY IMAGES
+// ==============================
+
+app.use(
+  "/category-images",
+  express.static(
+    path.join(__dirname, "uploads/categories")
+  )
+);
+
+// ==============================
+// TEST
+// ==============================
 
 app.get("/", (req, res) => {
-    res.send("Apple Blossom Server Running");
+  res.send("Apple Blossom Server Running");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
